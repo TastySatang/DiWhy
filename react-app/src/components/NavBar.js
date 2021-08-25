e364ba617a9bc63c253098cc0c785a77fcd24b4d
junyeoble <junyeoble@gmail.com>
2021-08-25 15:01:47 -0700

    more styling on homepage

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { NavLink, useHistory } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { logout } from '../store/session';
 
 import robot from './robot.png'
@@ -9,7 +9,6 @@ import './NavBar.css'
 
 const NavBar = () => {
   const dispatch = useDispatch()
-  const history = useHistory()
   const user = useSelector(state => state.session.user)
 
   const onLogout = async (e) => {
--- react-app/src/pages/Home/Homepage.js
+++ react-app/src/pages/Home/Homepage.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
+import back from './image1.jpg'
+import './Homepage.css'
+
 const HomePage = () => {
 
   return (
-    <h1>
-      Home Page
-    </h1>
+    <div>
+      <div className='Home__imageContent' style={{ backgroundImage: `url(${back})` }}>
+        <div className='imageContent__inner'>
+          <div className='imageContent__text'>
+            <h1>
+              YOURS FOR THE MAKING
+            </h1>
+            <p>
+              Instructables is a community for people who like to make things. Come explore, share, and make your next project with us!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='Home__textContent'>
+        <div className='textContent__box'>
+          <h2>Step-by-step</h2>
+          <p>We make it easy to learn how to make anything, one step at a time. From the stovetop to the workshop, you are sure to be inspired by the awesome projects that are shared everyday.</p>
+        </div>
+        <div className='textContent__box'>
+          <h2>Made By You</h2>
+          <p>Projects are created by you. No matter who you are, we all have secret skills to share. Come join our community of curious makers, innovators, teachers, and life long learners who love to share what they make.</p>
+        </div>
+        <div className='textContent__box'>
+          <h2>A Happy Place</h2>
+          <p>Making things makes people happy. We can't prove it, but we know it to be true. Find your happy place, and join one of the friendliest online communities anywhere.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
