a6dcbf210b2b479a3b3e30db7e0e9b994d7a7029
junyeoble <junyeoble@gmail.com>
2021-08-26 14:23:50 -0700

    asdf

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -14,6 +14,7 @@ const SignUpForm = () => {
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
+  // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
   const validateEmail = (mail) => {
     const regMail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
